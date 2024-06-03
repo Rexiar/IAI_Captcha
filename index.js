2c043a84d435af53501c838d5e7092b1c0d052e7
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-04 06:12:58 +0700

    feat: add landing page

--- index.js
+++ index.js
@@ -16,6 +16,33 @@ const captchaImages = fs.readdirSync(captchasDir).map(fileName => ({
 // Serve static files from the 'captchas' directory
 app.use('/captchas', express.static(captchasDir));
 
+app.get("/", (req, res) => {
+  res.send(`<main>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap');
+    main{
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+      min-height: 100vh;
+      font-family: 'Poppins', sans-serif;
+      color: #1B2430;
+    }
+    img {
+      width: 200px;
+    }
+    p, h2 {
+      text-align: center;
+    }
+  </style>
+  <div>
+    <h2>Custom Captcha Service</h2>
+    <p>Ver 1.0</p>
+  </div>
+</main>`);
+})
+
 // API endpoint to get captcha
 app.get('/captcha', (req, res) => {
   const randomIndex = Math.floor(Math.random() * captchaImages.length);
